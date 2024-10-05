4986ab788254cd853356ce99543fa331a8a6f9ca
Kartik <kartikkumarorkk@gmail.com>
2024-10-05 17:43:54 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorhandler } from '../util/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup=async (req,res,next)=>{
     const {username,email,password}=req.body;
@@ -22,6 +23,37 @@ export const signup=async (req,res,next)=>{
     res.json({message:"signup successful"})
     }catch(err){
     next(err)
+    }
+};
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email || !password || email=='' || password==''){
+        return next(errorhandler(400,'all fields required'));
+    }
+    try{
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            next(errorhandler(400,'invalid email'));
+        }
+        const validPass=bcryptjs.compareSync(password,validUser.password);
+        if(!validPass){
+            return next(errorhandler(400,'invalid password'));
+        }
+        const token=jwt.sign(
+            {
+                id:validUser._id,
+            },
+            process.env.JWT_SECRET_KEY
+        )
+        const { password : pass , ...rest}=validUser._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json(rest);
+    }
+    
+    catch(error){
+        throw error;
     }
 };
     
